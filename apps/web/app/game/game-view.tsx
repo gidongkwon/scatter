72968a501c6b929c82a371e27c92b1cf695410d1
Gidong Kwon <lucidfext@gmail.com>
2024-08-18 21:48:23 +0900

    spawn bullet

--- apps/web/app/game/game-view.tsx
+++ apps/web/app/game/game-view.tsx
@@ -8,17 +8,36 @@ import { Timer } from "@scatter/engine/timer/timer";
 export function GameView() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const engine = useEngine(canvasRef, (engine) => {
-    const textures = [
-      engine.assets.loadImage("/shooter/playerShip1_blue.png"),
-      engine.assets.loadImage("/shooter/playerShip2_green.png"),
-      engine.assets.loadImage("/shooter/playerShip3_red.png"),
-    ];
+    const playerTexture = engine.assets.loadImage(
+      "/shooter/playerShip1_blue.png",
+    );
+    const enemyTexture = engine.assets.loadImage(
+      "/shooter/playerShip3_red.png",
+    );
+    const playerBulletTexture = engine.assets.loadImage(
+      "/shooter/Lasers/laserBlue01.png",
+    );
 
     // TODO: implement proper id management system or better component design
     const TransformId = 0;
     const SpriteId = 1;
+    const VelocityId = engine.world.registerComponent();
+    interface Velocity {
+      x: number;
+      y: number;
+    }
+
     const PlayerId = engine.world.registerComponent();
+    interface Player {
+      score: number;
+    }
+
     const EnemyId = engine.world.registerComponent();
+
+    const BulletShooterId = engine.world.registerComponent();
+    interface BulletShooter {
+      delayTimer: Timer;
+    }
 
     engine.world.addSystem("init", (context) => {
       context.spawn([
@@ -37,41 +56,88 @@ export function GameView() {
         ],
         [
           SpriteId,
-          { textureInfo: textures[0], width: 1, height: 1 } satisfies Sprite,
+          { textureInfo: playerTexture, width: 1, height: 1 } satisfies Sprite,
         ],
-        [PlayerId, true],
+        [PlayerId, { score: 0 } satisfies Player],
+        [
+          BulletShooterId,
+          {
+            delayTimer: new Timer(0.1, { type: "once" }),
+          } satisfies BulletShooter,
+        ],
       ]);
     });
 
     const playerMoveSystem: System = (context) => {
+      context.each([TransformId, SpriteId, PlayerId], (_, rawComponents) => {
+        const [transform] = rawComponents as unknown as [Transform];
+
+        const speed = 300;
+        if (context.keyboard.isPressed("ArrowLeft")) {
+          transform.position.x -= speed * context.deltaTime;
+        }
+        if (context.keyboard.isPressed("ArrowRight")) {
+          transform.position.x += speed * context.deltaTime;
+        }
+        if (context.keyboard.isPressed("ArrowUp")) {
+          transform.position.y -= speed * context.deltaTime;
+        }
+        if (context.keyboard.isPressed("ArrowDown")) {
+          transform.position.y += speed * context.deltaTime;
+        }
+      });
+    };
+
+    const playerShooterSystem: System = (context) => {
       context.each(
-        [TransformId, SpriteId, PlayerId],
-        (entity, rawComponents) => {
-          const [transform, sprite] = rawComponents as unknown as [
+        [TransformId, BulletShooterId, PlayerId],
+        (_, rawComponents) => {
+          const [playerTransform, bulletShooter] = rawComponents as unknown as [
             Transform,
-            Sprite,
+            BulletShooter,
           ];
+          bulletShooter.delayTimer.tick(context.deltaTime);
+          if (!bulletShooter.delayTimer.finished) {
+            return;
+          }
 
-          console.log(entity, rawComponents);
-
-          const speed = 300;
-          if (context.keyboard.isPressed("ArrowLeft")) {
-            transform.position.x -= speed * context.deltaTime;
-          }
-          if (context.keyboard.isPressed("ArrowRight")) {
-            transform.position.x += speed * context.deltaTime;
-          }
-          if (context.keyboard.isPressed("ArrowUp")) {
-            transform.position.y -= speed * context.deltaTime;
-          }
-          if (context.keyboard.isPressed("ArrowDown")) {
-            transform.position.y += speed * context.deltaTime;
+          if (context.keyboard.isPressed("KeyZ")) {
+            context.spawn([
+              [
+                SpriteId,
+                {
+                  textureInfo: playerBulletTexture,
+                  width: 1,
+                  height: 1,
+                } satisfies Sprite,
+              ],
+              [
+                TransformId,
+                {
+                  position: {
+                    x: playerTransform.position.x,
+                    y: playerTransform.position.y,
+                  },
+                  scale: {
+                    x: 1,
+                    y: 1,
+                  },
+                } satisfies Transform,
+              ],
+              [
+                VelocityId,
+                {
+                  x: 500,
+                  y: 0,
+                } satisfies Velocity,
+              ],
+            ]);
           }
         },
       );
     };
 
-    const timer = new Timer(3, { type: "infinite" });
+    const timer = new Timer(1, { type: "infinite" });
     const enemySpawnSystem: System = (context) => {
       timer.tick(context.deltaTime);
       if (timer.segmentFinished) {
@@ -80,8 +146,8 @@ export function GameView() {
             TransformId,
             {
               position: {
-                x: 100,
-                y: 100,
+                x: Math.random() * context.stageWidth,
+                y: Math.random() * context.stageHeight,
               },
               scale: {
                 x: 1,
@@ -91,15 +157,34 @@ export function GameView() {
           ],
           [
             SpriteId,
-            { textureInfo: textures[1], width: 1, height: 1 } satisfies Sprite,
+            { textureInfo: enemyTexture, width: 1, height: 1 } satisfies Sprite,
           ],
           [EnemyId, true],
+          [
+            BulletShooterId,
+            {
+              delayTimer: new Timer(0.5, { type: "once" }),
+            } satisfies BulletShooter,
+          ],
         ]);
       }
     };
 
+    const velocitySystem: System = (context) => {
+      context.each([VelocityId, TransformId], (_, rawComponents) => {
+        const [velocity, transform] = rawComponents as unknown as [
+          Velocity,
+          Transform,
+        ];
+        transform.position.x += velocity.x * context.deltaTime;
+        transform.position.y += velocity.y * context.deltaTime;
+      });
+    };
+
     engine.world.addSystem("update", playerMoveSystem);
+    engine.world.addSystem("update", playerShooterSystem);
     engine.world.addSystem("update", enemySpawnSystem);
+    engine.world.addSystem("update", velocitySystem);
   });
 
   return (
